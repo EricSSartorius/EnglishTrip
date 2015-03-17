d1d0811dc965f1d095265dfead37dbb7a3d275ea
EricSSartorius <EricSSartorius@gmail.com>
2015-03-17 12:03:53 +0900

    carousel added to blog page

--- archive.php
+++ archive.php
@@ -4,35 +4,29 @@
       <div class="row">
         <div class="col-md-9">
           
+            <div class="page-header">
+              <h1><?php wp_title(''); ?></h1>
+            </div>
+
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <div class="page-header">
-          
-
-      <?php 
-        $thumbnail_id = get_post_thumbnail_id();
-        $thumbnail_url = wp_get_attachment_image_src ( $thumbnail_id, 'thumbnail-size', true );
-        $thumbnail_meta = get_post_meta( $thumnbnail_id. '_wp_attachment_image_alt', true);
-      ?>  
-
-      <p class="featured-image"><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>"></p>
-    
-     <h1><?php the_title(); ?></h1>
-
-
+            <article class="post">
+              <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
               <p><em>
                 By <?php the_author(); ?> 
                 on <?php echo the_time('l, F, jS, Y'); ?>
                 in <?php the_category( ', '); ?>.
                 <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
               </em></p>
-            </div>
 
-            <?php the_content(); ?>
+              
 
-            <hr>
+              <?php the_excerpt(); ?>
 
-            <?php comments_template(); ?>
+              <hr>
+
+            </article>
+
 
           <?php endwhile; else: ?>
 
@@ -46,7 +40,7 @@
 
         </div>
 
-        <?php get_sidebar( 'blog' ); ?>
+        <?php get_sidebar('blog'); ?>
       
       </div>
 
