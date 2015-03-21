a8d726cc4318f3dd1a9b4ff0b931dfff6b211eb3
EricSSartorius <EricSSartorius@gmail.com>
2015-03-21 15:19:53 +0900

    minor temp styling for clarification, dummy content added, carosel put on front page

--- header.php
+++ header.php
@@ -18,7 +18,10 @@
 
   <body <?php body_class(); ?>>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+
+
+
+    <nav class="navbar navbar-inverse">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -27,7 +30,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
+          <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><img src="<?php bloginfo('template_directory');?>/images/ETlogo.png" alt="ET Logo"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
          
