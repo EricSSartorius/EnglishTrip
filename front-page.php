0348932fcb0dcffc572809ba6510fda8afcd6674
EricSSartorius <EricSSartorius@gmail.com>
2015-04-14 15:24:28 +0900

    social icons added, minor header tweaks done

--- front-page.php
+++ front-page.php
@@ -85,4 +85,15 @@
 
 
 
-<?php get_footer(); ?>
+ </div> <!-- /container -->
+      <footer class="text-center static-footer">
+          의정부시 신곡1동 722-1번지 장암현대3차 동신A 상가2층 English Trip</br>
+      T.031)873-2014 &bull; info@englishtrip.net</br>
+        <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
+      </footer>
+   
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
--- header.php
+++ header.php
@@ -18,31 +18,46 @@
 
   <body <?php body_class(); ?>>
 
+    <!-- TOP-NAV -->
+    <div class="container-fluid main-container">
+      <div class="row">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-default">
 
-    <div class="row">
-      <div class="col-lg-12">
-       <div class="user-status">
-          <?php
-          if (is_user_logged_in()) {
-          $user = wp_get_current_user();
-          echo wp_loginout();
-          } else { ?>
-          <strong><?php wp_loginout(); ?></strong>
-          
-          <?php } ?>
-          </div> <!-- end user status -->
+            <ul class="nav navbar-nav navbar-right user-status">
+                <li class="hidden-xs">
+                    <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/FB.png" alt="Facebook Icon"></a>
+                </li>
+                <li class="hidden-xs">
+                    <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/NV.png" alt="Naver Icon"></a>
+                </li>
+                <li class="hidden-xs">
+                    <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/KK.png" alt="Kakao Icon"></a>
+                </li>
+                <li>
+                    <?php
+                    if (is_user_logged_in()) {
+                    $user = wp_get_current_user();
+                    echo wp_loginout();
+                    } else { ?>
+                    <strong><?php wp_loginout(); ?></strong>
+                    <?php } ?>
+                </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
-    
 
-    <div class="container main-container text-center">
-      <a class="visible-xs" href="<?php echo home_url(); ?>">
-            <img src="<?php bloginfo('template_directory');?>/images/etlogo.png" alt="ET Logo 2" class="etlogo">
+    <!-- MAIN-NAV -->
+    <div class="container main-container">
+      <a class="visible-xs text-center" href="<?php echo home_url(); ?>">
+            <img src="<?php bloginfo('template_directory');?>/images/etlogo.png" alt="ET Logo 2">
           </a>
       <nav class="navbar navbar-inverse" role="navigation">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <button type="button" class="navbar-toggle nav-button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
