961b87b4b26e9d316b642b0d83823ad5d7dfa728
EricSSartorius <EricSSartorius@gmail.com>
2015-04-20 20:41:47 +0900

    header updated

--- header.php
+++ header.php
@@ -18,60 +18,59 @@
 
   <body <?php body_class(); ?>>
 
-    <!-- TOP-NAV -->
-    <div class="container main-container">
-      <div class="row">
 
-  <div class="col-sm-6">
- <a class="hidden-xs" href="<?php echo home_url(); ?>">
-            <img class="et-logo" src="<?php bloginfo('template_directory');?>/images/et-logo.png" alt="ET Logo">
+  
+    <!-- TOP-NAV -->
+    <div class="container main-container et-menu">
+      <div class="row">
+        <div class="col-sm-6">
+          <a class="hidden-xs" href="<?php echo home_url(); ?>">
+            <img class="et-logo" src="<?php bloginfo('template_directory');?>/images/etlogo.png" alt="ET Logo">
           </a>
-</div>
-<div class="row">
-        <div class="col-sm-6 ">
+        </div>
+
+        <div class="col-sm-6 hidden-xs">
           <nav class="navbar navbar-default">
 
             <ul class="nav navbar-nav navbar-right user-status">
-                <li class="hidden-xs">
+                <li>
                     <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/FB.png" alt="Facebook Icon"></a>
                 </li>
-                <li class="hidden-xs">
+                <li>
                     <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/NV.png" alt="Naver Icon"></a>
                 </li>
-                <li class="hidden-xs">
+                <li>
                     <a href="#"><img src="<?php bloginfo('template_directory');?>/images/social/KK.png" alt="Kakao Icon"></a>
                 </li>
-                <li>
+                <li class="login-top">
                     <?php
                     if (is_user_logged_in()) {
                     $user = wp_get_current_user();
                     echo wp_loginout();
                     } else { ?>
-                    <strong><?php wp_loginout(); ?></strong>
+                    <?php wp_loginout(); ?>
                     <?php } ?>
                 </li>
             </ul>
           </nav>
         </div>
-   
 
     <!-- MAIN-NAV -->
     
         <div class="col-md-6">
-      <a class="visible-xs text-center" href="<?php echo home_url(); ?>">
-            <img src="<?php bloginfo('template_directory');?>/images/etlogo.png" alt="ET Logo 2">
+          <a class="visible-xs text-center" href="<?php echo home_url(); ?>">
+            <img class="etlogo2" src="<?php bloginfo('template_directory');?>/images/etlogo.png" alt="ET Logo 2">
           </a>
-      <nav class="navbar navbar-inverse" role="navigation">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle nav-button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-         
-      </div>
+          <nav class="navbar navbar-inverse" role="navigation">
+          <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle nav-button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button> 
+           </div>
   
           <?php
               wp_nav_menu( array(
@@ -86,12 +85,12 @@
                   'walker'            => new wp_bootstrap_navwalker())
               );
           ?>
+       
+          </nav>
+        </div>
+      </div>
     </div>
-</nav>
-
-</div>
-</div>
-
+ 
 
 
 
